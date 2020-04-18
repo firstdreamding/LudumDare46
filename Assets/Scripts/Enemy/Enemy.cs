ed8294017bd05926896ca39b6d618d06f9e50b20
gemeLn <mtang317@gmail.com>
2020-04-18 13:16:14 -0500

    Enemy changes

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,39 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum State
+{
+    KNOCKBACK,
+    OFFPATH,
+    ONPATH
+}
 public class Enemy : MonoBehaviour
 {
 
-    int hp = 10;
+    public int hp;
+    public float speed;
 
-    // Start is called before the first frame update
+    //don't hardcode these, defined in functions
+    private Vector2 resetPoint;
+    private Vector2 knockbackPoint;
+    private float knockbackSpeed;
+
+
+    private State state;
     void Start()
     {
-        
+        state = State.ONPATH;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 temp = transform.localPosition;
-        temp.x += 0.01f;
-        transform.localPosition = temp;
         if (hp == 0)
         {
             Destroy(gameObject);
         }
     }
-
+    private void FixedUpdate()
+    {
+        switch (state)
+        {
+            case State.OFFPATH:
+                transform.position = Vector3.MoveTowards(transform.position, resetPoint, speed);
+                if (Vector3.Distance(transform.position, resetPoint) < 0.001f)
+                {
+                    state = State.ONPATH;
+                }
+                break;
+            case State.ONPATH:
+                transform.localPosition += new Vector3(speed, 0, 0);
+                break;
+            case State.KNOCKBACK:
+                transform.position = Vector3.MoveTowards(transform.position, knockbackPoint, knockbackSpeed);
+                if (Vector3.Distance(transform.position, knockbackPoint) < 0.001f)
+                {
+                    state = State.OFFPATH;
+                }
+                break;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.tag == "Projectile")
         {
             Debug.Log("Ouch");
-            hp = (int) (hp - collision.GetComponent<Projectile>().GetDamage());
-        } else
+            hp = (int)(hp - collision.GetComponent<Projectile>().GetDamage());
+        }
+        else
         {
             Debug.Log("else");
         }
+    }
+    public void Attacked(int damage, Vector3 attackerPos, float knockbackConstant)
+    {
+        hp -= damage;
+        Vector3 dispalcement = knockbackConstant * (transform.position - attackerPos).normalized;
+        knockbackPoint = transform.position + dispalcement;
+        knockbackSpeed = 5 * knockbackConstant;
+        resetPoint = transform.position;
+        state = State.KNOCKBACK;
     }
 }
--- Assets/Scripts/Scythe.cs
+++ Assets/Scripts/Scythe.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class Scythe : MonoBehaviour
 {
+    public float lifetime;
+    public int damage;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 0.25f);
+        Destroy(gameObject, lifetime);
     }
     public void doTransform(int dir)
     {
-        
+
         Vector3 offset = new Vector3(dir == 1 ? 1 : dir == 3 ? -1 : 0, dir == 0 ? 1 : dir == 2 ? -1 : 0, -1);
-        transform.localPosition = 0.5f * offset;
-        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90 * (dir + 1)));
+        transform.localPosition = 0.15f * offset;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -90 * (dir - 1)));
     }
     // Update is called once per frame
     void Update()
     {
 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<Enemy>().Attacked(damage, transform.position, 1);
+        }
     }
 }
