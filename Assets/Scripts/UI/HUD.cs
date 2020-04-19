8e9c28724c729b4882c86c6e2dc41c87902b890d
gemeLn <mtang317@gmail.com>
2020-04-18 21:02:59 -0500

    902 eat my

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    Text goldText;
+    Text waveText;
+    public void setGold(int count)
+    {
+        goldText.text = "GOLD: " + count;
+    }
+    public void setWave(int wave)
+    {
+        waveText.text = "WAVE: " + wave;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        goldText = transform.Find("Gold").GetComponent<Text>();
+        waveText = transform.Find("Wave").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
